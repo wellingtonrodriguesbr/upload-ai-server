dd7b2a87adf3884b46092f2cb1735adc9440bf44
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-10-03 19:59:01 -0300

    fix: fix routes

--- src/http/controllers/routes.ts
+++ src/http/controllers/routes.ts
@@ -3,6 +3,6 @@ import { UploadVideoController } from "./upload-video";
 import { getAllPromptsController } from "./get-all-prompts";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post("/videos", UploadVideoController);
+  // app.post("/videos", UploadVideoController);
   app.get("/prompts", getAllPromptsController);
 }
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import fastifyCors from "@fastify/cors";
 import fastifyMultipart from "@fastify/multipart";
 import { createTranscriptionRoute } from "./routes/create-transcription";
 import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
+import { uploadVideoRoute } from "./routes/upload-video";
 import { appRoutes } from "./http/controllers/routes";
 
 const app = fastify();
@@ -23,6 +24,7 @@ app.register(fastifyMultipart, {
 });
 
 app.register(appRoutes);
+app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
 app.register(generateAiCompletionRoute);
 
